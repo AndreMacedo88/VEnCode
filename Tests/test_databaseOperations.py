257bf1cd8a09c6d98ed87a1d1987baa5d1e4372e
AndreMacedo88 <AndreMacedo88@users.noreply.github.com>
2017-05-04 13:47:09 +0100

    Add files via upload

---
+++ Tests/test_databaseOperations.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from VEnCode_FANTOM5 import Classes_2
+
+promoters = Classes_2.Promoters("hg19.cage_peak_phase1and2combined_tpm.osc.txt",)
+prom_data = promoters
+
+class TestDatabaseOperations(TestCase):
+    def test_at_least_one_node_calculator_2(self):
+        self.assertEqual(Classes_2.DatabaseOperations.at_least_one_node_calculator_2())
+        self.fail()
